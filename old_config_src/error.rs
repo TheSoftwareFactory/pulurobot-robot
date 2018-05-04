a883b15463fadc89f381775ad027828bfed9007d
Brian Alberg <xirov@xirov.com>
2018-05-04 15:35:04 +0300

    Deleted old config code

--- old_config_src/error.rs
+++
@@ -1,9 +0,0 @@
-use super::{ConfigErrorHandler, ConfigError, ConfigErrorType};
-
-impl ConfigErrorHandler for ConfigError {
-    fn new(error_type: ConfigErrorType) -> Self {
-        ConfigError {
-            err_type: error_type,
-        }
-    }
-}
